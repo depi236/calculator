b024694a5d2707e14cef1107e96ab0cc0de9245e
Nozhak Andrii <voodoowarloks@gmail.com>
2022-11-22 18:55:45 +0200

    Class CalcManager prototype

--- CalcManager.cs
+++ CalcManager.cs
@@ -4,6 +4,30 @@ namespace Calculator
 {
     class CalcManager
     {
+        public double add(double x,double y)
+        {
+            return x+y;            
+        }
 
+         public double sub(double x,double y)
+        {
+            return x-y;            
+        }
+
+         public double mul(double x,double y)
+        {
+            return x*y;            
+        }
+         public double div(double x,double y)
+        {
+            if(y!=0)
+            {
+                return x/y;
+            }
+            else   
+            {
+                return 0;                
+            }         
+        }
     }
 }
